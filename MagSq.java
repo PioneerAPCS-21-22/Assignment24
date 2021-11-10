fe5db657556e17c5f995ded5e564afc4d8cb008d
codytheking <codytheking93@gmail.com>
2021-11-10 11:19:51 -0800

    Code outline

---
+++ MagSq.java
@@ -0,0 +1,36 @@
+
+pubic class MagSq
+{
+    public static void main(String[] agrgs)
+    {
+        
+    }
+    
+    /**
+     *  The first n magic squares have been printed 
+     *  (with squared number and integer sum indicated)
+     *  Ex: 36 (1 to 8)
+     *
+     * @param n - the number of magic squares to be printed
+     */
+    public static void printSquares(int n)
+    {
+        // square, sum, addend, sqrt
+        // 1^2, 2^2, 3^2, ... (sqrt = 1, 2, 3, ...)
+
+        // for loop n times
+        
+            // while square != sum
+                // increment addend
+                // add it to the sum
+
+                // check if sum is past square
+                    // increment sqrt
+                    // update square
+
+            // print the square and last addend
+            // increment sqrt
+            // update square
+    }
+}
+
